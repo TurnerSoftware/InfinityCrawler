eedd5d73ec8d283c6a7c68c14544ceec2f22fe37
Turnerj <git@turnerj.com>
2019-01-01 00:58:39 +1030

    TestBase allowing for internal logging etc

---
+++ tests/InfinityCrawler.Tests/TestBase.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace InfinityCrawler.Tests
+{
+	[TestClass]
+	public class TestBase
+	{
+		private ILoggerFactory LoggerFactory { get; }
+
+		public TestBase()
+		{
+			var serviceProvider = new ServiceCollection()
+				.AddLogging(builder =>
+				{
+					builder.AddFilter("InfinityCrawler", LogLevel.Trace);
+					builder.AddConsole();
+					builder.AddDebug();
+				})
+				.BuildServiceProvider();
+
+			LoggerFactory = serviceProvider.GetService<ILoggerFactory>();
+		}
+
+		protected ILogger<T> GetLogger<T>()
+		{
+			return LoggerFactory.CreateLogger<T>();
+		}
+	}
+}
