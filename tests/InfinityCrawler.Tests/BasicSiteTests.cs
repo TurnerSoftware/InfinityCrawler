6884abc4b304e62090202bd4eb1885879f679599
Turnerj <git@turnerj.com>
2019-01-01 15:14:06 +1030

    Adding initial tests for a "basic" site

--- tests/InfinityCrawler.Tests/BasicSiteTests.cs
+++ tests/InfinityCrawler.Tests/BasicSiteTests.cs
@@ -10,16 +10,79 @@ namespace InfinityCrawler.Tests
 	[TestClass]
 	public class BasicSiteTests : TestBase
 	{
-		[TestMethod, Timeout(10000)]
-		public async Task BasicSiteTest()
+		private async Task<CrawlResult> GetCrawlResult()
 		{
 			var crawler = GetTestSiteCrawler(new SiteContext
 			{
 				SiteFolder = "BasicSite"
 			});
 			var settings = GetFastCrawlTestSettings();
+			return await crawler.Crawl(new Uri("http://localhost/"), settings);
+		}
 
-			var result = await crawler.Crawl(new Uri("http://localhost/"), settings);
+		[TestMethod]
+		public async Task DiscoverIndexPageFromSitemap()
+		{
+			var result = await GetCrawlResult();
+			var uri = new Uri("http://localhost/index.html");
+			Assert.IsTrue(result.CrawledUris.Any(c => c.Location == uri));
+		}
+
+		[TestMethod]
+		public async Task CrawledLinksOnIndexPage()
+		{
+			var result = await GetCrawlResult();
+			var uri = new Uri("http://localhost/malformed.html");
+			Assert.IsTrue(result.CrawledUris.Any(c => c.Location == uri));
+		}
+
+		[TestMethod]
+		public async Task FoundLinkFromMalformedPaged()
+		{
+			var result = await GetCrawlResult();
+			var uri = new Uri("http://localhost/malformed-child.html");
+			Assert.IsTrue(result.CrawledUris.Any(c => c.Location == uri));
+		}
+
+		[TestMethod]
+		public async Task ObeysRobotsBlocking()
+		{
+			var result = await GetCrawlResult();
+			var uri = new Uri("http://localhost/robots-blocked.html");
+			var robotsBlockedResult = result.CrawledUris.Where(c => c.Location == uri).FirstOrDefault();
+
+			var robotsChildUri = new Uri("http://localhost/robots-blocked-childs.html");
+
+			Assert.AreEqual(CrawlStatus.RobotsBlocked, robotsBlockedResult.Status);
+			Assert.IsFalse(result.CrawledUris.Any(c => c.Location == robotsChildUri));
+		}
+
+		[TestMethod]
+		public async Task UrisOnlyAppearOnceInResults()
+		{
+			var result = await GetCrawlResult();
+			var uri = new Uri("http://localhost/index.html");
+			Assert.AreEqual(1, result.CrawledUris.Count(c => c.Location == uri));
+		}
+
+		[TestMethod]
+		public async Task UrisAreRetriedOnServerErrors()
+		{
+			var result = await GetCrawlResult();
+			var uri = new Uri("http://localhost/status/500");
+			var crawledUri = result.CrawledUris.Where(c => c.Location == uri).FirstOrDefault();
+			Assert.AreEqual(3, crawledUri.Requests.Count);
+		}
+
+		[TestMethod]
+		public async Task UrisAreNotRetriedOn4xxErrors()
+		{
+			var result = await GetCrawlResult();
+			var uris = new[]
+			{
+				new Uri("http://localhost/status/401")
+			};
+			Assert.IsTrue(uris.All(uri => result.CrawledUris.Any(c => c.Location == uri && c.Requests.Count == 1)));
 		}
 	}
 }
