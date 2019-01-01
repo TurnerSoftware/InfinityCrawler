bbd7d488a5a2b55aa9ef9c6ff14d0e0c0fc5dc19
Turnerj <git@turnerj.com>
2019-01-01 21:06:03 +1030

    Initial benchmark for basic site

---
+++ tests/InfinityCrawler.Tests.Benchmarks/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using BenchmarkDotNet.Running;
+
+namespace InfinityCrawler.Tests.Benchmarks
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			BenchmarkRunner.Run<BasicSiteCrawlBenchmark>();
+		}
+	}
+}
